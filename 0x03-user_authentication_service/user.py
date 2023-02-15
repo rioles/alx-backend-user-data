49d88a8feae98a1f70f9841e74cf62bc2e88030e
rioles <rioles1992@gmail.com>
2023-02-15 18:00:17 +0100

    create user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-from sqlalchemy import Column, Integer, String
+""" User model
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
 Base = declarative_base()
 
 
 class User(Base):
+    """ SQLAlchemy User model
+    """
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
